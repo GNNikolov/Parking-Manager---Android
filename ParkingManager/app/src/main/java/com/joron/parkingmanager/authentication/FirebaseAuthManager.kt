700fab267b1300aca39b3aa7394a431f661b8cf1
Joro <nikolov@alfastack.bg>
2020-09-02 20:38:52 +0200

    Code optimizations, added test phone credentials to test

--- ParkingManager/app/src/main/java/com/joron/parkingmanager/authentication/FirebaseAuthManager.kt
+++ ParkingManager/app/src/main/java/com/joron/parkingmanager/authentication/FirebaseAuthManager.kt
@@ -2,8 +2,10 @@ package com.joron.parkingmanager.authentication
 
 import androidx.fragment.app.FragmentActivity
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import com.joron.parkingmanager.viewmodel.CarViewModel
 import com.joron.parkingmanager.viewmodel.UserAuthViewModel
+import java.util.*
 
 /**
  * Created by Joro on 14/07/2020
@@ -11,9 +13,16 @@ import com.joron.parkingmanager.viewmodel.UserAuthViewModel
 class FirebaseAuthManager(private val context: FragmentActivity,
                           private val authViewModel: UserAuthViewModel,
                           private val carViewModel: CarViewModel) {
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    val firebaseAuthSettings = firebaseAuth.firebaseAuthSettings.also {
+        it.setAutoRetrievedSmsCodeForPhoneNumber("+359886556085", "293233")
+    }
+
 
     private val providers = arrayListOf(
-        AuthUI.IdpConfig.PhoneBuilder().build()
+        AuthUI.IdpConfig.PhoneBuilder()
+            .setWhitelistedCountries(Collections.singletonList("BG"))
+            .build()
     )
 
     fun signIn() = context.startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN)
--- ParkingManager/app/src/main/java/com/joron/parkingmanager/networking/RequestSendCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.joron.parkingmanager.networking
-
-/**
- * Created by Joro on 27/08/2020
- */
-interface RequestSendCallback {
-    fun onError(code: Int)
-    fun onLoading()
-}
